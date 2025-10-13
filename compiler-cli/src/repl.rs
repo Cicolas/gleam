6f0f1c518c291084cd4defdf3534feab3482c421
Nícolas Carvalho <nicolas.mnw@gmail.com>
2025-10-12 22:17:42 -0300

    implements Erlang REPL

--- compiler-cli/src/repl.rs
+++ compiler-cli/src/repl.rs
@@ -1,6 +1,6 @@
 use camino::Utf8Path;
 use expectrl::{
-    Expect, Session,
+    Expect, Regex, Session,
     process::unix::{PtyStream, UnixProcess},
     stream::log::LogStream,
 };
@@ -21,15 +21,17 @@ use gleam_core::{
 
 use rustyline::{DefaultEditor, error::ReadlineError};
 use tempfile::{self, TempPath};
+use tracing_subscriber::fmt::format;
 
 use std::{
     cell::RefCell,
     collections::HashMap,
+    fmt::Write,
     path::PathBuf,
     process::Command,
     rc::Rc,
-    fmt::Write,
     sync::mpsc::{self, Sender},
+    thread,
     time::Duration,
 };
 
@@ -58,9 +60,10 @@ const TYPE: &str = ":type ";
 
 // FIXME: use echo template file
 const GLEAM_REPL_MJS: &[u8] = include_bytes!("gleam_repl.mjs");
+const GLEAM_REPL_ERL: &[u8] = include_bytes!("gleam_repl.erl");
 
 const REPL_MAIN: &str = "repl_main";
-const REPL_FNS: &str = r#"
+const REPL_JS_FNS: &str = r#"
 @external(javascript, "../gleam_repl.mjs", "repl_save")
 pub fn repl_save(value: a) -> a
 
@@ -70,14 +73,25 @@ pub fn repl_load(index: Int) -> a
 @external(javascript, "../gleam_repl.mjs", "repl_print")
 pub fn repl_print(value: a) -> a
 "#;
-const GLEAM_BREAK_CODE: &str = "\u{0204}\u{0169}\u{0156}\u{006E}\u{0068}\u{00FA}\u{0014}\u{01B7}\u{01DD}\u{0077}\u{0014}\u{022D}\u{018D}\u{020D}\u{0118}\u{013A}\u{00B7}\u{015C}\u{01B8}\u{0118}\u{0057}\u{0159}\u{00AB}\u{0047}\u{00BF}\u{0005}\u{002C}\u{00A3}\u{01F5}\u{00FE}\u{00CA}\u{0009}";
+const REPL_ERL_FNS: &str = r#"
+@external(erlang, "gleam_repl", "repl_save")
+pub fn repl_save(value: a) -> a
+
+@external(erlang, "gleam_repl", "repl_load")
+pub fn repl_load(index: Int) -> a
+
+@external(erlang, "gleam_repl", "repl_print")
+pub fn repl_print(value: a) -> a
+"#;
+// const GLEAM_BREAK_CODE: &str = "\u{0204}\u{0169}\u{0156}\u{006E}\u{0068}\u{00FA}\u{0014}\u{01B7}\u{01DD}\u{0077}\u{0014}\u{022D}\u{018D}\u{020D}\u{0118}\u{013A}\u{00B7}\u{015C}\u{01B8}\u{0118}\u{0057}\u{0159}\u{00AB}\u{0047}\u{00BF}\u{0005}\u{002C}\u{00A3}\u{01F5}\u{00FE}\u{00CA}\u{0009}";
+const GLEAM_BREAK_CODE: &str = "GlEaM";
 
 pub fn command(paths: &ProjectPaths) -> Result<(), Error> {
     let built = build_with_progress(paths)?;
     let package = &built.root_package.config.name;
     let module = built.module_interfaces.get(package);
 
-    let mut repl = Repl::<Deno>::new(paths.clone(), package.into(), module).unwrap();
+    let mut repl = Repl::<Erlang>::new(paths.clone(), package.into(), module).unwrap();
 
     let mut editor = DefaultEditor::new().unwrap();
     if let Some(history) = &history_path() {
@@ -137,7 +151,7 @@ fn build(paths: &ProjectPaths, progress: bool, warnings: bool) -> Result<Built, 
             codegen: Codegen::All,
             compile: Compile::All,
             mode: Mode::Dev,
-            target: Some(Target::JavaScript),
+            target: Some(Target::Erlang),
             no_print_progress: !progress,
         },
         crate::build::download_dependencies(paths, cli::Reporter::new())?,
@@ -165,7 +179,7 @@ impl std::io::Write for EventWriter {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         let text = String::from_utf8_lossy(buf);
 
-        // self.sender.send(text.to_string()).ok().unwrap();
+        self.sender.send(text.to_string()).ok().unwrap();
 
         Ok(buf.len())
     }
@@ -174,66 +188,81 @@ impl std::io::Write for EventWriter {
         Ok(())
     }
 }
-type DenoSession = Session<UnixProcess, LogStream<PtyStream, EventWriter>>;
+type ErlangSession = Session<UnixProcess, LogStream<PtyStream, EventWriter>>;
 
 #[derive(Clone)]
-struct Deno {
-    session: Rc<RefCell<DenoSession>>,
+struct Erlang {
+    session: Rc<RefCell<ErlangSession>>,
     paths: ProjectPaths,
     package: String,
 }
 
-impl Engine for Deno {
+impl Engine for Erlang {
     fn new(paths: ProjectPaths, package: String) -> Self {
-        let mut command = Command::new("deno");
+        let mut command = Command::new("erl");
         let _ = command
             // .arg("-i")
             .env("TERM", "dumb")
-            .env("NO_COLOR", "1")
-            .arg("repl")
-            .arg("--allow-read")
-            .arg("--quiet");
+            .env("NO_COLOR", "1");
 
         let (sender, receiver) = mpsc::channel::<String>();
 
         // TODO: add a observer thread
-        // let _ = thread::spawn(move || {
-        // let mut last_command: String = "".into();
-        // let mut is_read = false;
+        let _ = thread::spawn(move || {
+            // let mut last_command: String = "".into();
+            // let mut is_read = false;
 
-        // println!("[Event System] Listening for events...");
-        // for received_event in receiver {
-        // if received_event == "write" || received_event == "read" {
-        //     is_read = false;
-        // }
+            // println!("[Event System] Listening for events...");
+            for received_event in receiver {
+                // if received_event == "write" || received_event == "read" {
+                //     is_read = false;
+                // }
 
-        // // println!("isread: {}, received_event: {}, last_command: {}", is_read, received_event, last_command);
-        // if is_read && received_event != "\"" {
-        //     print!("{received_event}");
-        // }
+                // // println!("isread: {}, received_event: {}, last_command: {}", is_read, received_event, last_command);
+                // if is_read && received_event != "\"" {
+                //     print!("{received_event}");
+                // }
 
-        // if received_event == ": " && last_command == "read" {
-        //     is_read = true;
-        // }
+                // if received_event == ": " && last_command == "read" {
+                //     is_read = true;
+                // }
 
-        // print!(" '{received_event}' ");
-        // last_command = received_event.clone();
-        // }
-        // println!("\n[Event System] Stopped listening.");
-        // });
+                // print!(" '{received_event}' ");
+                // last_command = received_event.clone();
+            }
+            // println!("\n[Event System] Stopped listening.");
+        });
 
         let event_writer = EventWriter { sender };
 
         let mut session = expectrl::session::log(
-            Session::spawn(command).expect("Could not spawn deno subprocess"),
+            Session::spawn(command).expect("Could not spawn Erlang subprocess"),
             event_writer,
         )
         .expect("Unable to log into eventWritter()");
 
-        session.set_expect_timeout(Some(Duration::from_secs(5)));
-        let _ = session.expect("> ").unwrap();
+        let build_dir = paths.build_directory_for_package(Mode::Dev, Target::Erlang, &package);
 
-        Deno {
+        session.set_expect_timeout(Some(Duration::from_secs(5)));
+
+        let _ = session.expect(Regex("\\d+> ")).unwrap();
+        session
+            .send_line("code:add_patha(\"build/dev/erlang/gleam_stdlib/ebin/\").")
+            .unwrap();
+        let _ = session.expect(Regex("\\d+> ")).unwrap();
+        session
+            .send_line(format!(
+                "code:add_patha(\"build/dev/erlang/{package}/ebin/\")."
+            ))
+            .unwrap();
+        let _ = session.expect(Regex("\\d+> ")).unwrap();
+        session
+            .send_line(format!(
+                "c(\"{build_dir}/_gleam_artefacts/gleam_repl\", [{{outdir, \"{build_dir}/ebin\"}}])."
+            )).unwrap();
+        let _ = session.expect(Regex("\\d+> ")).unwrap();
+
+        Erlang {
             session: Rc::new(RefCell::new(session)),
             paths,
             package,
@@ -243,36 +272,47 @@ impl Engine for Deno {
     fn run_main(&mut self, module: &str) {
         let path = self
             .paths
-            .build_directory_for_target(Mode::Dev, Target::JavaScript)
+            .build_directory_for_target(Mode::Dev, Target::Erlang)
             .join(&self.package)
-            .join(format!("{module}.mjs"));
+            .join(format!("{module}.erl"));
 
         let mut session = self.session.borrow_mut();
 
         let mut code = format!("
-                import {{ {REPL_MAIN} }} from \"{path}\";
+            try
+                {module}:repl_main(),
+                '{GLEAM_BREAK_CODE}'
+            catch error:#{{
+                    gleam_error := GleamError,
+                    module := Module,
+                    function := Function,
+                    line := Line
+                }} ->
+                    io:format(
+                        standard_error,
+                        \"Error at ~s.~s:~p\\n  Gleam error: ~p\\n\",
+                        [Module, Function, Line, GleamError]
+                    ), '{GLEAM_BREAK_CODE}';
 
-                try {{
-                    {REPL_MAIN}();
-                }} catch (err) {{
-                    if (err.gleam_error) {{
-                        console.error(
-                            `Error at ${{err.module}}.${{err.function}}:${{err.line}}\n    Gleam error: ${{err.gleam_error}}`
-                        );
-                    }} else {{
-                        console.error({{err}});
-                    }}
-                }} finally {{
-                    console.log(\"{GLEAM_BREAK_CODE}\");
-                }}");
-        code = code.lines().map(|line| line.trim()).collect();
+                Class:Reason ->
+                    io:format(standard_error, \"~p~n\", [{{Class, Reason}}]),
+                    '{GLEAM_BREAK_CODE}'
+            end.");
+        code = code.lines().map(|line| {
+            let trimmed_line = line.trim();
+            format!("{trimmed_line} ")
+        }).collect();
 
-        let _ = session.send_line(code).unwrap();
+        let _ = session
+            .send_line(code)
+            .unwrap();
 
-        let res = session.expect(GLEAM_BREAK_CODE).unwrap();
-        let _ = session.expect("> ");
+        let mut buf = "".to_owned();
+        let res = session.expect(format!("\'{GLEAM_BREAK_CODE}\'")).unwrap();
+        buf.push_str(&String::from_utf8_lossy(res.before()));
+        let res = session.expect(Regex("\\d+> ")).unwrap();
+        buf.push_str(&String::from_utf8_lossy(res.before()));
 
-        let buf = String::from_utf8_lossy(res.before());
         let trimmed = buf.trim();
 
         println!("{trimmed}");
@@ -282,14 +322,15 @@ impl Engine for Deno {
         let mut session = self.session.borrow_mut();
 
         let code =
-            format!("globalThis.repl_vars && {index} < globalThis.repl_vars.length || false");
+            format!("gleam_repl:repl_has_var({index}).");
 
         session.send_line(code).unwrap();
 
-        let res = session.expect("> ").unwrap();
+        let res = session.expect(Regex("\\d+> ")).unwrap();
 
         let buf = String::from_utf8_lossy(res.before());
         let trimmed = buf.trim();
+
         match trimmed {
             "true" => return true,
             "false" => return false,
@@ -328,9 +369,12 @@ impl<E: Engine> Repl<E> {
         module: Option<&ModuleInterface>,
     ) -> Result<Repl<E>, Error> {
         let project = ProjectIO::new();
-        let build_dir = paths.build_directory_for_target(Mode::Dev, Target::JavaScript);
+        let build_dir = paths.build_directory_for_package(Mode::Dev, Target::Erlang, &package);
         project
-            .write_bytes(&build_dir.join("gleam_repl.mjs"), GLEAM_REPL_MJS)
+            .write_bytes(
+                &build_dir.join("_gleam_artefacts").join("gleam_repl.erl"),
+                GLEAM_REPL_ERL,
+            )
             .unwrap();
         Ok(Repl {
             user_import: module.map(import_public_types_and_values),
@@ -396,7 +440,7 @@ impl<E: Engine> Repl<E> {
 
     fn build_source(&self) -> String {
         let mut src = String::new();
-        src.push_str(REPL_FNS);
+        src.push_str(REPL_ERL_FNS);
         self.add_imports(&mut src);
         self.add_consts(&mut src);
         self.add_types(&mut src);
